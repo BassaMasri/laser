09a643baaecbee9c1703a9ee4a062c8a365d9a59
BassaMasri <55700888+BassaMasri@users.noreply.github.com>
2023-01-21 12:36:43 +0000

    websocket

---
+++ Crypto/templates/app.js
@@ -0,0 +1,11 @@
+let ws = new WebSocket('wss://stream.binance.com:9443/ws/btcusdt@trade');  //dogeusdt
+let stockPriceElement = document.getElementById('stock-price');
+let lastPrice = null;
+
+ws.onmessage = (event) => { // onmessage should be assigned to a function, not invoked with parenthesis
+    let stockObject = JSON.parse(event.data);
+    let price = parseFloat(stockObject.p).toFixed(5);
+    stockPriceElement.innerText = price; // update the value of innerText with the price variable
+    stockPriceElement.style.color = !lastPrice || lastPrice === price ? 'black' : price > lastPrice ? 'green' : 'red';
+    lastPrice = price;
+};
