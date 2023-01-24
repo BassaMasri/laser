e8ea17e2af2a33b562927e35785b9e57d934447b
BassaMasri <55700888+BassaMasri@users.noreply.github.com>
2023-01-24 21:33:37 +0000

    index

--- Crypto/templates/assets/js/app.js
+++ Crypto/templates/assets/js/app.js
@@ -1,33 +1,33 @@
-let wsbtc = new WebSocket('wss://stream.binance.com:9443/ws/btcusdt@trade');  //dogeusdt
+let wsbtc = new WebSocket('wss://stream.binance.com:9443/ws/btcusdt@trade');
 let BtcPriceElement = document.getElementById('stock-price-btc');
-let wseth = new WebSocket('wss://stream.binance.com:9443/ws/dogeusdt@trade');
-let EthPriceElement = document.getElementById('stock-price-doge');
+let wsdoge = new WebSocket('wss://stream.binance.com:9443/ws/dogeusdt@trade');
+let DogePriceElement = document.getElementById('stock-price-doge');
 let wsbnb = new WebSocket('wss://stream.binance.com:9443/ws/bnbusdt@trade');
 let BnbPriceElement = document.getElementById('stock-price-bnb');
 let lastPrice = null;
 
-wsbtc.onmessage = (event) => { // onmessage should be assigned to a function, not invoked with parenthesis
+wsbtc.onmessage = (event) => {
     let stockObject = JSON.parse(event.data);
     let price = parseFloat(stockObject.p).toFixed(1);
-    BtcPriceElement.innerText = price; // update the value of innerText with the price variable
+    BtcPriceElement.innerText = price;
     BtcPriceElement.style.color = !lastPrice || lastPrice === price ? 'white' : price > lastPrice ? 'green' : 'red';
     lastPrice = price;
 };
 
 
-wseth.onmessage = (event) => { // onmessage should be assigned to a function, not invoked with parenthesis
+wsdoge.onmessage = (event) => {
     let stockObject = JSON.parse(event.data);
     let price = parseFloat(stockObject.p).toFixed(5);
-    EthPriceElement.innerText = price; // update the value of innerText with the price variable
-    EthPriceElement.style.color = !lastPrice || lastPrice === price ? 'white' : price > lastPrice ? 'green' : 'red';
+    DogePriceElement.innerText = price;
+    DogePriceElement.style.color = !lastPrice || lastPrice === price ? 'white' : price > lastPrice ? 'green' : 'red';
     lastPrice = price;
 };
 
 
-wsbnb.onmessage = (event) => { // onmessage should be assigned to a function, not invoked with parenthesis
+wsbnb.onmessage = (event) => {
     let stockObject = JSON.parse(event.data);
     let price = parseFloat(stockObject.p).toFixed(1);
-    BnbPriceElement.innerText = price; // update the value of innerText with the price variable
+    BnbPriceElement.innerText = price;
     BnbPriceElement.style.color = !lastPrice || lastPrice === price ? 'white' : price > lastPrice ? 'green' : 'red';
     lastPrice = price;
 };
