b347342b1e949e66d5faffa48e947ae9a70b2c45
BassaMasri <55700888+BassaMasri@users.noreply.github.com>
2023-01-24 21:27:57 +0000

    model

--- Crypto/cryptoapp/models.py
+++ Crypto/cryptoapp/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 
 class Coin(models.Model):
+    def __str__(self):
+        return self.name
+    
+    name = models.CharField(max_length=20)
     open = models.CharField(max_length=20)
     close = models.CharField(max_length=20)
     hight = models.CharField(max_length=20)
     low = models.CharField(max_length=20)
-    volume = models.CharField(max_length=20)
+
+    
