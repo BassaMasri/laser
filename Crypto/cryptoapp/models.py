d9a85adbb10e342ef75b650e9d4d887d5b0c42cb
BassaMasri <55700888+BassaMasri@users.noreply.github.com>
2023-01-25 00:17:40 +0200

    Symb

--- Crypto/cryptoapp/models.py
+++ Crypto/cryptoapp/models.py
@@ -2,9 +2,10 @@ from django.db import models
 
 class Coin(models.Model):
     def __str__(self):
-        return self.name
-    
-    name = models.CharField(max_length=20)
+        return self.symbol
+
+    symbol = models.CharField(max_length=10)
+    name = models.CharField(max_length=10)
     open = models.CharField(max_length=20)
     close = models.CharField(max_length=20)
     hight = models.CharField(max_length=20)
